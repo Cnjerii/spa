b0c3e8def5af33a8b3fda4e8a3ec9cd9f29bfac5
Cnjerii <christinenjeri162@gmail.com>
2022-07-28 11:14:46 +0300

    styling

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-
 urlpatterns = [
-    path('', views.Home),
-    path('about/', views.about),
-    path('services/', views.services),
-    path('contactus/', views.contactus),
+    path('', views.Home, name='home'),
+    path('about/', views.about, name='about'),
+    path('services/', views.services, name='services'),
+    path('contact-us/', views.contactus, name='contact-us'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
