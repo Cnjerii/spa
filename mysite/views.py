3284958506e2fde5e7a43904f8a8c7187e99be70
Cnjerii <christinenjeri162@gmail.com>
2022-08-02 16:22:35 +0300

    contact

--- mysite/views.py
+++ mysite/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.core.mail import send_mail
 # Create your views here.
 
 def Home(request):
@@ -13,24 +13,24 @@ def services(request):
 	return render(request, 'mysite/services.html')
 
 def contactus(request):
+	if request.method == 'POST':
+		print('REQUEST DATA:', request.POST)
+		name = request.POST.get('your-name')
+		email = request.POST.get('your-email')
+		subject = request.POST.get('subject')
+		message = request.POST.get('message')
+
+		data = {
+		'name': name,
+		'email': email,
+		'subject': subject,
+		'message': message
+		}
+		message = '''
+		New message: {}
+
+		FROM: {}
+		'''.format(data['message'], data['email'])
+		send_mail(data['subject'], message, '', ['christinenjeri162@gmail.com'])
+		return render(request, 'mysite/dashboard.html')
 	return render(request, 'mysite/contact.html')
-
-	#if request.method == 'POST':
-		#name = request.POST.get('your-name')
-		#email = request.POST.get('your-email')
-		#subject = request.POST.get('subject')
-		#message = request.POST.get('message')
-
-		#data = {
-		#'name': name,
-		#'email': email,
-		#'subject': subject,
-		#'message': message
-		#}
-		#message = '''
-		#New message: {}
-
-		#FROM: {}
-		#'''.format(data['message'], data['email'])
-		#send_mail(data['subject'], message, '', ['christinenjeri162@gmail.com'])
-		#return render(request, 'mysite/dashboard.html')
